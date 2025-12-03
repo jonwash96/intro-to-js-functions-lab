7ec7329c67cfc74cc6a0668beb32828f0dfe4c35
JON Washington <jonwashPro@gmail.com>
2025-12-03 16:10:16 -0600

    feat: Page Content

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-// EXERCISE 1 MAX OF 2 NUMBERS TERNARY STYLE
+// DOM Elements (cards)
+const ex1 = document.getElementById('ex1')
+const ex2 = document.getElementById('ex2')
+const ex3 = document.getElementById('ex3')
+const ex4 = document.getElementById('ex4')
+const ex5 = document.getElementById('ex5')
+const ex6 = document.getElementById('ex6')
+const ex7 = document.getElementById('ex7')
+const ex8 = document.getElementById('ex8')
+const ex9 = document.getElementById('ex9')
+
+// EXERCISE 1 - MAX OF 2 NUMBERS TERNARY STYLE
 const maxOf2Numbers = (x,y) => x>y ? x : y;
-console.log("Exercise 1 Result. Max of 2 numbers: ", maxOf2Numbers(3,9));
+print(ex1,{title:"Max of 2 Numbers", input:"3,9", func:maxOf2Numbers(3,9), proto:maxOf2Numbers})
 
 // EXERCISE 2
 const isAdult = (age) => age>18 ? true : false;
-console.log("Exercise 2 Result. Is Adult? ", isAdult(21));
+print(ex2,{title:"Is Adult?", input:"21", func:isAdult(21), proto:isAdult})
 
 // EXERCISE 3
 function isCharAVowel(char) {
@@ -14,26 +25,26 @@ function isCharAVowel(char) {
     if (vowels.test(char)) {return true;
     } else {return false}
 }
-console.log("Exercise 3 Result. Is the character a vowel? ", isCharAVowel('a'));
+print(ex3,{title:"Is Character A Vowel?", input:'a', func:isCharAVowel('a'), proto:isCharAVowel});
 
 // EXERCISE 4
 const generateEmail = (uname,domain) => `${uname}@${domain}`
-console.log("Exercise 4 Result. Email Generate an Email: ", generateEmail("johnsmith", "example.com"));
+print(ex4,{title:"Generate Email", input:"johnsmith, example.com", func:generateEmail("johnsmith", "example.com"), proto:generateEmail});
 
 // EXERCISE 5
 const greetUser = (fname,tod) => `Good ${tod}, ${fname}`;
-console.log("Exercise 5 Result. Greet User: ", greetUser("sam", "morning"));
+print(ex5,{title:"Greet User", input:"sam, morning", func:greetUser("sam", "morning"), proto:greetUser});
 
 // EXERCISE 6
 function maxOfThree(a,b,c) {
     if (arguments.length > 3 ) {console.warn("WARNING! (at Exercise 6) too many argumen ss. Only the first 3 were evaluated.")};
     return Math.max(a,b,c);
 }
-console.log("Exercise 6 Result. Max of 3 numbers ", maxOfThree(5,10,8));
+print(ex6,{title:"Max of 3 Numbers", input:"5,10,8", func:maxOfThree(5,10,8), proto:maxOfThree});
 
 // EXERCISE 7
 const calculateTip = (total,tip) => total * (tip/100);
-console.log("Exercise 7 Result. Calculate tip ", calculateTip(50,20));
+print(ex7,{title:"Calculate Tip", input:"50,20", func:calculateTip(50,20), proto:calculateTip});
 
 // EXERCISE 8
 function convertTemperature(num,unit) {
@@ -47,7 +58,7 @@ function convertTemperature(num,unit) {
         default: {return console.error("ERROR! (at Exercise 8) unit must be 'c' or 'f', case insensitive. Got ",unit)};
     }
 }
-console.log("Exerceise 8 Result. Convert Temperature: ",convertTemperature(32,'c'));
+print(ex8,{title:"Convert Temperature", input:"32,c", func:convertTemperature(32,'c'), proto:convertTemperature});
 
 // EXERCISE 9
 function basicCalculator(a,b,operator) {
@@ -59,5 +70,27 @@ function basicCalculator(a,b,operator) {
         default: {return console.error("ERROR! (at Exercise 9). Operator must be one of 'add|subtract|multiply|divide'. Got ",operator)}
     }
 }
-console.log("Exercise 9 Result. Basic Calculator: ", basicCalculator(10,5,'subtract'));
+print(ex9,{title:"Basic Calculator", input:"10,5,subtract", func:basicCalculator(10,5,'subtract'), proto:basicCalculator});
 
+
+// HELPER FUNCTIONS
+function print(el,obj) {
+    const ex = Number(el.id.match(/\d$/g)[0]); // Number
+    console.log(`Excercise ${ex} Result. ${obj.title}: `, obj.func);
+
+    const headding = document.createElement('h2');
+        headding.innerHTML = `Excercise ${ex}<br> "${obj.title}"`;
+        headding.style.textAlign = 'center';
+        el.appendChild(headding);
+    const result = document.createElement('p');
+        result.innerHTML = `Input: ${obj.input}<br>Result: ${obj.func}`;
+        el.appendChild(result);
+    const explanation = document.createElement('div');
+    const explanationTitle = document.createElement('h4');
+        explanationTitle.textContent = "Function Prototype:";
+        explanation.appendChild(explanationTitle);
+    const expanded = document.createElement('p');
+        expanded.textContent = obj.proto;
+        explanation.append(expanded);
+        el.appendChild(explanation);
+}   
